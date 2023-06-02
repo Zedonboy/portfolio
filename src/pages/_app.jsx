5bb8bd2a7045990aea0dff2d5846323bb81d0d8e
Declan Nnadozie <zedonbiz@gmail.com>
2023-06-02 20:35:07 +0100

    update

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,6 +2,7 @@ import { useEffect, useRef } from 'react'
 
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
+import { Analytics } from '@vercel/analytics/react';
 
 import '@/styles/tailwind.css'
 import 'focus-visible'
@@ -33,6 +34,7 @@ export default function App({ Component, pageProps, router }) {
         </main>
         <Footer />
       </div>
+      <Analytics/>
     </>
   )
 }
